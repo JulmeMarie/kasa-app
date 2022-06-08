30c2e3baabcc2579bed6e358da0b1ef1fa66b1cc
JulmeMarie <83822246+JulmeMarie@users.noreply.github.com>
2022-06-08 19:28:34 +0200

    correction : ajout d'une constante permettant de stocker les phrases statiques du site

--- src/pages/apropos/Apropos.js
+++ src/pages/apropos/Apropos.js
@@ -3,6 +3,7 @@ import Banner from "../../components/banner/Banner";
 import Accordion from "../../components/accordion/Accordion";
 import './Apropos.css';
 import Footer from "../../components/footer/Footer";
+import { staticData } from "../../Util";
 
 /**
  * Auteur : JULME Marie Wilnie
@@ -17,16 +18,16 @@ export default function Apropos() {
             <Banner bannerClass='banner-apropos'/>
             <div className="apropos accordion-list">
                 <div className='fiability-accordion'>
-                    <Accordion header="Fiabilité" body={['Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.']}/>
+                    <Accordion header="Fiabilité" body={[staticData.fiability]}/>
                 </div>
                 <div className='respect-accordion'>
-                        <Accordion header="Respect" body={['La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.']}/>
+                        <Accordion header="Respect" body={[staticData.respect]}/>
                 </div>
                 <div className='service-accordion'>
-                    <Accordion header="Service" body={['Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N\'hésitez pas à nous contacter si vous avez la moindre question.']}/>
+                    <Accordion header="Service" body={[staticData.service]}/>
                 </div>
                 <div className='security-accordion'>
-                        <Accordion header="Sécurité" body={['La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l\'hôte qu\'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.']}/>
+                        <Accordion header="Sécurité" body={[staticData.security]}/>
                 </div>
             </div>
         </div>
