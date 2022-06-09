f392d8360829bd18b311f29bd7bc10cfeb5c9851
JulmeMarie <83822246+JulmeMarie@users.noreply.github.com>
2022-06-09 13:08:31 +0200

    correction : création d'un fichier pour le gérer le routage, mise à jour du dossier et fichier de la page 404

--- src/Routing.js
+++ src/Routing.js
@@ -4,6 +4,10 @@ import Home from './pages/home/Home.js';
 import Apropos from './pages/apropos/Apropos';
 import FicheLogement from './pages/logement/FicheLogement';
 
+/**
+ * Auteur : JULME Marie Wilnie
+ * Date : 09/06/2022
+ */
 export default function Routing() {
     return(
         <BrowserRouter>
